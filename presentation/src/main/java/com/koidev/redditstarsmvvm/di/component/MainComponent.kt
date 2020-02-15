9fe9619ee77d2e86f040582ac63f3253a8980d59
DigitalKoi <tarasfreedev@gmail.com>
2020-02-15 23:08:32 +0100

    feature: Configured DI and Retrofit
    * added Device module for getting location or network state ant other
    * configured build.gradle for Presentation module
    
    [RSVM-000]

--- presentation/src/main/java/com/koidev/redditstarsmvvm/di/component/MainComponent.kt
+++ presentation/src/main/java/com/koidev/redditstarsmvvm/di/component/MainComponent.kt
@@ -1,11 +1,13 @@
 package com.koidev.redditstarsmvvm.di.component
 
 import com.koidev.redditstarsmvvm.di.module.AppModule
+import com.koidev.redditstarsmvvm.di.module.NetworkModule
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton @Component(modules = [
-    AppModule::class
+    AppModule::class,
+    NetworkModule::class
 ])
 interface MainComponent  {
 
